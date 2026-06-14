db6c881e2cd3f61b07f70795c44dfa7b22e5507b
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-14 17:21:53 +0530

    Time: 16 ms (10.16%), Space: 69.4 MB (9.11%) - LeetHub

--- 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
+++ 0236-lowest-common-ancestor-of-a-binary-tree/0236-lowest-common-ancestor-of-a-binary-tree.java
@@ -9,3 +9,24 @@ class Solution {
         return left != null ? left : right;
     }
 }
+
+//OR
+
+//TLE aayega(due to recursion)
+
+// class Solution {
+//     public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+//         if(root == null) return null;
+//         if(root == p || root == q) return root;
+//         boolean pLiesInLeft = exists(root.left , p);
+//         boolean qLiesInRight = exists(root.right , q);
+//         if(pLiesInLeft && !qLiesInRight) return lowestCommonAncestor(root.left , p ,q);
+//         else if(!pLiesInLeft && qLiesInRight) return lowestCommonAncestor(root.right , p ,q);
+//         else return root;
+//     }
+//     private boolean exists(TreeNode root , TreeNode val){
+//         if(root == null) return false;
+//         if(root == val) return true;
+//         return exists(root.left , val) || exists(root.right , val);
+//     }
+// }
