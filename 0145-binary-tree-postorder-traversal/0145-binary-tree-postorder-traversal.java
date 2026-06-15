7ee5da9e4c6b9212998213e392c5cbfe10f63430
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-15 15:19:22 +0530

    Time: 1 ms (13.3%), Space: 43 MB (83.9%) - LeetHub

--- 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
+++ 0145-binary-tree-postorder-traversal/0145-binary-tree-postorder-traversal.java
@@ -1,28 +1,32 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// class Solution {
+//     public void dfs(ArrayList<Integer> arr , TreeNode root){
+//         if(root == null) return;
+//         dfs(arr , root.left);
+//         dfs(arr , root.right);
+//         arr.add(root.val);
+//     }
+//     public List<Integer> postorderTraversal(TreeNode root) {
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         dfs(arr , root);
+//         return arr;   
+//     }
+// }
+
+//OR  (Iterative Approach)
+
 class Solution {
-    public void dfs(ArrayList<Integer> arr , TreeNode root){
-        if(root == null) return;
-        dfs(arr , root.left);
-        dfs(arr , root.right);
-        arr.add(root.val);
-    }
-    public List<Integer> postorderTraversal(TreeNode root) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        dfs(arr , root);
-        return arr;   
+    public List<Integer> postorderTraversal(TreeNode root){
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if(root == null) return ans;
+        st.push(root);
+        while(st.size() > 0){
+            TreeNode top = st.pop();
+            ans.add(top.val);
+            if(top.left != null) st.push(top.left);
+            if(top.right != null) st.push(top.right);
+        }
+        Collections.reverse(ans);
+        return ans;
     }
 }
