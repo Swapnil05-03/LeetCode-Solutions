01b66513b3177d5e2f822b2be47cf18b19ad169f
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-15 15:17:20 +0530

    Time: 1 ms (6.77%), Space: 42.9 MB (92.35%) - LeetHub

--- 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
+++ 0144-binary-tree-preorder-traversal/0144-binary-tree-preorder-traversal.java
@@ -1,28 +1,31 @@
-/**
- * Definition for a binary tree node.
- * public class TreeNode {
- *     int val;
- *     TreeNode left;
- *     TreeNode right;
- *     TreeNode() {}
- *     TreeNode(int val) { this.val = val; }
- *     TreeNode(int val, TreeNode left, TreeNode right) {
- *         this.val = val;
- *         this.left = left;
- *         this.right = right;
- *     }
- * }
- */
+// class Solution {
+//     public void dfs(ArrayList<Integer> arr , TreeNode root){
+//         if(root == null) return;
+//         arr.add(root.val);
+//         dfs(arr , root.left);
+//         dfs(arr , root.right);
+//     }
+//     public List<Integer> preorderTraversal(TreeNode root) {
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         dfs(arr , root);
+//         return arr;   
+//     }
+// }
+
+//OR  (Iterative Approach)
+
 class Solution {
-    public void dfs(ArrayList<Integer> arr , TreeNode root){
-        if(root == null) return;
-        arr.add(root.val);
-        dfs(arr , root.left);
-        dfs(arr , root.right);
-    }
-    public List<Integer> preorderTraversal(TreeNode root) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        dfs(arr , root);
-        return arr;   
+    public List<Integer> preorderTraversal(TreeNode root){
+        List<Integer> ans = new ArrayList<>();
+        Stack<TreeNode> st = new Stack<>();
+        if(root == null) return ans;
+        st.push(root);
+        while(st.size() > 0){
+            TreeNode top = st.pop();
+            ans.add(top.val);
+            if(top.right != null) st.push(top.right);
+            if(top.left != null) st.push(top.left);
+        }
+        return ans;
     }
 }
