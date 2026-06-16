00b64cf1eb45803d0ecb38a0a485b7a1c8e3fa4d
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-16 19:01:11 +0530

    Time: 1 ms (20.37%), Space: 47.6 MB (25.76%) - LeetHub

--- 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
+++ 0538-convert-bst-to-greater-tree/0538-convert-bst-to-greater-tree.java
@@ -1,15 +1,35 @@
-class Solution {
-    public static void inorder(TreeNode root , ArrayList<TreeNode> arr){
-        if(root == null) return;
-        inorder(root.left , arr);
-        arr.add(root);
-        inorder(root.right , arr);
-    }
+// class Solution {
+//     public static void inorder(TreeNode root , ArrayList<TreeNode> arr){
+//         if(root == null) return;
+//         inorder(root.left , arr);
+//         arr.add(root);
+//         inorder(root.right , arr);
+//     }
+//     public TreeNode convertBST(TreeNode root) {
+//         ArrayList<TreeNode> arr = new ArrayList<>();
+//         inorder(root , arr);
+//         Collections.reverse(arr);
+//         int sum = 0;
+//         for(int i = 0; i < arr.size() ; i++){
+//             sum += arr.get(i).val;
+//             arr.get(i).val = sum;
+//         }
+//         return root;
+//     }
+// }
 
+//OR
+
+class Solution {
+    public static void revInorder(TreeNode root , ArrayList<TreeNode> arr){
+        if(root == null) return;
+        revInorder(root.right , arr);
+        arr.add(root);
+        revInorder(root.left , arr);
+    }
     public TreeNode convertBST(TreeNode root) {
         ArrayList<TreeNode> arr = new ArrayList<>();
-        inorder(root , arr);
-        Collections.reverse(arr);
+        revInorder(root , arr);
         int sum = 0;
         for(int i = 0; i < arr.size() ; i++){
             sum += arr.get(i).val;
