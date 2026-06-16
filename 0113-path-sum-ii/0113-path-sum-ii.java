3bf79ea62ae7287c6049343601a0b2ef65acdaa5
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-16 11:21:53 +0530

    Time: 1 ms (99.99%), Space: 45.4 MB (72.29%) - LeetHub

---
+++ 0113-path-sum-ii/0113-path-sum-ii.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<List<Integer>> pathSum(TreeNode root, int sum) {
+        List<List<Integer>> ans = new ArrayList<>();
+        List<Integer> arr = new ArrayList<>();
+        dfs(root , ans , arr , sum);
+        return ans;
+    }
+    private static void dfs(TreeNode root , List<List<Integer>> ans , List<Integer> arr , int sum){
+        if(root == null) return;
+        arr.add(root.val);
+         if (root.left == null && root.right == null && sum == root.val) {
+            ans.add(new ArrayList<>(arr));
+        }
+        if(root.left != null) dfs(root.left , ans , arr , sum-root.val);
+        if(root.right != null) dfs(root.right , ans , arr , sum-root.val);
+        
+        arr.remove(arr.size() - 1);
+    }
+}
