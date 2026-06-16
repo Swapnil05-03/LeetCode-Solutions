4fa1a4bcd9077f4c76f7226e9a792ad6fb4ddcbd
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-16 16:52:41 +0530

    Time: 0 ms (100%), Space: 45 MB (77.81%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.java
@@ -1,25 +1,36 @@
+// class Solution {
+//     public boolean isSorted(ArrayList<Integer> arr) {
+//         for (int i = 1; i < arr.size(); i++) {
+//             if (arr.get(i) <= arr.get(i - 1)) {
+//                 return false;
+//             }
+//         }
+//         return true;
+//     }
+//     public void inorder(TreeNode root , ArrayList<Integer> arr){
+//         if(root == null) return;
+//         inorder(root.left , arr);
+//         arr.add(root.val);
+//         inorder(root.right , arr);
+//     }
+    
+    
+//     public boolean isValidBST(TreeNode root) {
+//         ArrayList<Integer> arr = new ArrayList<>();
+//         inorder(root , arr);
+//         boolean flag = isSorted(arr);
+//         if(flag) return true;
+//         else return false;
+//     }
+// }
+
 class Solution {
-    public boolean isSorted(ArrayList<Integer> arr) {
-        for (int i = 1; i < arr.size(); i++) {
-            if (arr.get(i) <= arr.get(i - 1)) {
-                return false;
-            }
-        }
-        return true;
-    }
-    public void inorder(TreeNode root , ArrayList<Integer> arr){
-        if(root == null) return;
-        inorder(root.left , arr);
-        arr.add(root.val);
-        inorder(root.right , arr);
-    }
-    
-    
-    public boolean isValidBST(TreeNode root) {
-        ArrayList<Integer> arr = new ArrayList<>();
-        inorder(root , arr);
-        boolean flag = isSorted(arr);
-        if(flag) return true;
-        else return false;
+    TreeNode prev = null;
+     public boolean isValidBST(TreeNode root){
+        if(root == null) return true;
+        if(!isValidBST(root.left)) return false;
+        if(prev != null && root.val <= prev.val) return false;
+        prev = root;
+        return isValidBST(root.right);
     }
 }
