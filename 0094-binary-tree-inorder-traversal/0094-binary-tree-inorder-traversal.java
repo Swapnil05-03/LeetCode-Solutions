9f7366bbd734282aa5c967da8990332bde37cb96
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-17 17:44:21 +0530

    Time: 0 ms (100%), Space: 43 MB (69.09%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,3 +1,37 @@
+//Morris Traversal
+
+class Solution {
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        TreeNode curr = root;
+        while(curr != null){
+            if(curr.left != null){
+                //find predecessor and do work
+                TreeNode pred = curr.left;
+                while(pred.right != null && pred.right != curr) pred = pred.right; 
+                if(pred.right == null){
+                    //link
+                    pred.right = curr;
+                    curr = curr.left;
+                }
+                else{ // pred.right == curr --> Unlink and print
+                    pred.right = null;
+                    ans.add(curr.val);
+                    curr = curr.right;
+                }
+            }
+            else{
+                ans.add(curr.val);
+                curr = curr.right;
+            }
+        } 
+        return ans;
+    }
+}
+
+
+//OR
+
 // class Solution {
 //     public void dfs(ArrayList<Integer> arr , TreeNode root){
 //         if(root == null) return;
@@ -14,26 +48,26 @@
 
 //OR (Iterative Approach)
 
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        Stack<TreeNode> st = new Stack<>();
-        TreeNode curr = root;
-        while (st.size() > 0 || curr != null) {
-            if (curr != null) {
-                if (curr.left != null) {
-                    st.push(curr);
-                    curr = curr.left;
-                } else {
-                    ans.add(curr.val);
-                    curr = curr.right;
-                }
-            } else { // curr == null
-                TreeNode top = st.pop();
-                ans.add(top.val);
-                curr = top.right;
-            }
-        }
-        return ans;
-    }
-}
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         Stack<TreeNode> st = new Stack<>();
+//         TreeNode curr = root;
+//         while (st.size() > 0 || curr != null) {
+//             if (curr != null) {
+//                 if (curr.left != null) {
+//                     st.push(curr);
+//                     curr = curr.left;
+//                 } else {
+//                     ans.add(curr.val);
+//                     curr = curr.right;
+//                 }
+//             } else { // curr == null
+//                 TreeNode top = st.pop();
+//                 ans.add(top.val);
+//                 curr = top.right;
+//             }
+//         }
+//         return ans;
+//     }
+// }
