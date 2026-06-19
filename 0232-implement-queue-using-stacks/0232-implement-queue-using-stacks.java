39926f847de2aec23f8537d510d28143321773c5
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-19 10:39:32 +0530

    Time: 0 ms (100%), Space: 42.6 MB (79.13%) - LeetHub

--- 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
+++ 0232-implement-queue-using-stacks/0232-implement-queue-using-stacks.java
@@ -7,47 +7,47 @@ class MyQueue {
     }
     
     public void push(int x) {
-        // st.push(x);
+        st.push(x);
 
         //or
 
-        while(st.size() > 0){
-            helper.push(st.pop());
-        }
-        st.push(x);
-        while(helper.size() > 0){
-            st.push(helper.pop());
-        }
+        // while(st.size() > 0){
+        //     helper.push(st.pop());
+        // }
+        // st.push(x);
+        // while(helper.size() > 0){
+        //     st.push(helper.pop());
+        // }
     }
     
     public int pop() {
-        return st.pop();
+        // return st.pop();
 
         //or
 
-        // while(st.size() > 1){
-        //     helper.push(st.pop());
-        // }
-        // int front = st.pop();
-        // while(helper.size() > 0){
-        //     st.push(helper.pop());
-        // }
-        // return front;
+        while(st.size() > 1){
+            helper.push(st.pop());
+        }
+        int front = st.pop();
+        while(helper.size() > 0){
+            st.push(helper.pop());
+        }
+        return front;
     }
     
     public int peek() {
-        return st.peek();
+        // return st.peek();
 
         //or
 
-        // while(st.size() > 1){
-        //     helper.push(st.pop());
-        // }
-        // int front = st.peek();
-        // while(helper.size() > 0){
-        //     st.push(helper.pop());
-        // }
-        // return front;
+        while(st.size() > 1){
+            helper.push(st.pop());
+        }
+        int front = st.peek();
+        while(helper.size() > 0){
+            st.push(helper.pop());
+        }
+        return front;
     }
     
     public boolean empty() {
