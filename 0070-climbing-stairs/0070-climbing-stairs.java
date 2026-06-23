a78b8422ff0ec77c14bad6853e5977e95d4f5307
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-23 15:50:46 +0530

    Time: 0 ms (100%), Space: 42.4 MB (17.01%) - LeetHub

---
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -0,0 +1,10 @@
+class Solution {
+    int[] arr = new int[46];
+    public int climbStairs(int n) {
+        if(n<=2) return n;
+
+        if(arr[n] != 0) return arr[n];
+        arr[n] = climbStairs(n-1) + climbStairs(n-2);
+        return arr[n];    
+    }
+}
