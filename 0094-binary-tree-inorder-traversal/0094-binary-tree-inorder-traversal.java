ae20151e01722836ab203f190744c16ecd855f36
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-23 10:35:58 +0530

    Time: 0 ms (100%), Space: 43 MB (69.47%) - LeetHub

--- 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
+++ 0094-binary-tree-inorder-traversal/0094-binary-tree-inorder-traversal.java
@@ -1,50 +1,50 @@
 //Morris Traversal
-
-class Solution {
-    public List<Integer> inorderTraversal(TreeNode root) {
-        List<Integer> ans = new ArrayList<>();
-        TreeNode curr = root;
-        while(curr != null){
-            if(curr.left != null){
-                //find predecessor and do work
-                TreeNode pred = curr.left;
-                while(pred.right != null && pred.right != curr) pred = pred.right; 
-                if(pred.right == null){
-                    //link
-                    pred.right = curr;
-                    curr = curr.left;
-                }
-                else{ // pred.right == curr --> Unlink and print
-                    pred.right = null;
-                    ans.add(curr.val);
-                    curr = curr.right;
-                }
-            }
-            else{
-                ans.add(curr.val);
-                curr = curr.right;
-            }
-        } 
-        return ans;
-    }
-}
+//T.C --> O(n) and A.S --> O(1)
+// class Solution {
+//     public List<Integer> inorderTraversal(TreeNode root) {
+//         List<Integer> ans = new ArrayList<>();
+//         TreeNode curr = root;
+//         while(curr != null){
+//             if(curr.left != null){
+//                 //find predecessor and do work
+//                 TreeNode pred = curr.left;
+//                 while(pred.right != null && pred.right != curr) pred = pred.right; 
+//                 if(pred.right == null){
+//                     //link
+//                     pred.right = curr;
+//                     curr = curr.left;
+//                 }
+//                 else{ // pred.right == curr --> Unlink and print
+//                     pred.right = null;
+//                     ans.add(curr.val);
+//                     curr = curr.right;
+//                 }
+//             }
+//             else{
+//                 ans.add(curr.val);
+//                 curr = curr.right;
+//             }
+//         } 
+//         return ans;
+//     }
+// }
 
 
 //OR
 
-// class Solution {
-//     public void dfs(ArrayList<Integer> arr , TreeNode root){
-//         if(root == null) return;
-//         dfs(arr , root.left);
-//         arr.add(root.val);
-//         dfs(arr , root.right);
-//     }
-//     public List<Integer> inorderTraversal(TreeNode root) {
-//         ArrayList<Integer> arr = new ArrayList<>();
-//         dfs(arr , root);
-//         return arr;   
-//     }
-// }
+class Solution {
+    public void dfs(ArrayList<Integer> arr , TreeNode root){
+        if(root == null) return;
+        dfs(arr , root.left);
+        arr.add(root.val);
+        dfs(arr , root.right);
+    }
+    public List<Integer> inorderTraversal(TreeNode root) {
+        ArrayList<Integer> arr = new ArrayList<>();
+        dfs(arr , root);
+        return arr;   
+    }
+}
 
 //OR (Iterative Approach)
 
