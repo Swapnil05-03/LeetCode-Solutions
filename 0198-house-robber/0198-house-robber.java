04fe4ac6caf747cd05bba673d480ab5868b756a5
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-24 14:24:55 +0530

    Time: 0 ms (100%), Space: 42.6 MB (73.7%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,16 +1,31 @@
 class Solution {
-    static int[] dp;
     public int rob(int[] arr) {
         int n = arr.length;
-        dp = new int[n]; //0 to n-1
-        Arrays.fill(dp , -1);
-        return loot(0 , arr);
-    }
-    public int loot(int i , int[] arr){ // i -> 0 to n-1
-        if(i >= arr.length) return 0;
-        if(dp[i] != -1) return dp[i];
-        int pick = arr[i] + loot(i+2 , arr);
-        int skip = loot(i+1 , arr);
-        return dp[i] = Math.max(pick , skip);
+        int[] dp = new int[n];
+        dp[0] = arr[0];
+        if(n > 1) dp[1] = Math.max(arr[0] , arr[1]);
+        for(int i = 2; i < n; i++){
+            dp[i] = Math.max(arr[i] + dp[i-2] , dp[i-1]);
+        }
+        return dp[n-1];
     }
 }
+
+//OR
+
+// class Solution {
+//     static int[] dp;
+//     public int rob(int[] arr) {
+//         int n = arr.length;
+//         dp = new int[n]; //0 to n-1
+//         Arrays.fill(dp , -1);
+//         return loot(0 , arr);
+//     }
+//     public int loot(int i , int[] arr){ // i -> 0 to n-1
+//         if(i >= arr.length) return 0;
+//         if(dp[i] != -1) return dp[i];
+//         int pick = arr[i] + loot(i+2 , arr);
+//         int skip = loot(i+1 , arr);
+//         return dp[i] = Math.max(pick , skip);
+//     }
+// }
