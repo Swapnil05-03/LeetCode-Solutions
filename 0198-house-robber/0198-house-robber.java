497da843eaa79e5f85520fff2006bac7e8aae12b
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-25 16:04:15 +0530

    Time: 0 ms (100%), Space: 43 MB (5.28%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,18 +1,38 @@
+//Space Optimization
 class Solution {
     public int rob(int[] arr) {
         int n = arr.length;
-        int[] dp = new int[n];
+        int[] dp = new int[3];
         dp[0] = arr[0];
+        if(n==1) return arr[0];
         if(n > 1) dp[1] = Math.max(arr[0] , arr[1]);
+        if(n==2) return dp[1];
         for(int i = 2; i < n; i++){
-            dp[i] = Math.max(arr[i] + dp[i-2] , dp[i-1]);
+            dp[2] = Math.max(arr[i] + dp[0] , dp[1]);
+            dp[0] = dp[1];
+            dp[1] = dp[2];
         }
-        return dp[n-1];
+        return dp[2];
     }
 }
 
+//Tabulation
+// class Solution {
+//     public int rob(int[] arr) {
+//         int n = arr.length;
+//         int[] dp = new int[n];
+//         dp[0] = arr[0];
+//         if(n > 1) dp[1] = Math.max(arr[0] , arr[1]);
+//         for(int i = 2; i < n; i++){
+//             dp[i] = Math.max(arr[i] + dp[i-2] , dp[i-1]);
+//         }
+//         return dp[n-1];
+//     }
+// }
+
 //OR
 
+//Memoization
 // class Solution {
 //     static int[] dp;
 //     public int rob(int[] arr) {
