292053045f68bc68bcbd734b6bf1c5e9201f7aff
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-25 15:28:55 +0530

    Time: 0 ms (100%), Space: 42 MB (51.53%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,10 +1,26 @@
+//Tabulation
+// class Solution {
+//     public int fib(int n) {
+//         int[] dp = new int[n+1];
+//         if(n>=1) dp[1] = 1; //n ==0 ke case mein dp[1] exist hi nahi krega na
+//         for(int i =2; i<=n; i++){
+//             dp[i] = dp[i-1] + dp[i-2];
+//         }
+//         return dp[n];
+//     }
+// }
+
+//Space Optimization
 class Solution {
     public int fib(int n) {
-        int[] dp = new int[n+1];
-        if(n>=1) dp[1] = 1; //n ==0 ke case mein dp[1] exist hi nahi krega na
+        if(n<=1) return n;
+        int[] dp = new int[3];
+        dp[1] = 1;
         for(int i =2; i<=n; i++){
-            dp[i] = dp[i-1] + dp[i-2];
+            dp[2] = dp[1] + dp[0];
+            dp[0] = dp[1];
+            dp[1] = dp[2]; 
         }
-        return dp[n];
+        return dp[2];
     }
 }
