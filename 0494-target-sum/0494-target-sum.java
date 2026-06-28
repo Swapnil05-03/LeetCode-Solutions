57dbdf782b3587b08f1b42e72cd0935d78f5f236
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-28 14:34:33 +0530

    Time: 259 ms (31.36%), Space: 47.1 MB (14.99%) - LeetHub

--- 0494-target-sum/0494-target-sum.java
+++ 0494-target-sum/0494-target-sum.java
@@ -1,14 +1,38 @@
+//Memoization
 class Solution {
+    static int absSum;
     public int findTargetSumWays(int[] arr, int target) {
-        return helper(0 , target , arr);
+        absSum = 0;
+        for(int ele : arr) absSum += Math.abs(ele);
+        int[][] dp = new int[arr.length][2*absSum+1];
+        return helper(0 ,0 , target , arr , dp);
     }
-    public int helper(int i , int target , int[] arr){
+    public int helper(int i ,int sum , int target , int[] arr ,int[][] dp){
         if(i == arr.length){
-            if(target == 0) return 1; // 1 valid way
+            if(sum == target) return 1; // 1 valid way
             else return 0;
         }
-        int add = helper(i+1 , target-arr[i] , arr);
-        int subtract = helper(i+1 , target+arr[i] , arr);
-        return add + subtract;
+        if(dp[i][sum + absSum] != 0) return dp[i][sum+absSum];
+        int add = helper(i+1 , sum + arr[i], target , arr , dp);
+        int subtract = helper(i+1 , sum - arr[i] , target , arr , dp);
+        return dp[i][sum+absSum] = add + subtract;
     }
 }
+
+//OR
+
+//Recursive Way
+// class Solution {
+//     public int findTargetSumWays(int[] arr, int target) {
+//         return helper(0 , target , arr);
+//     }
+//     public int helper(int i , int target , int[] arr){
+//         if(i == arr.length){
+//             if(target == 0) return 1; // 1 valid way
+//             else return 0;
+//         }
+//         int add = helper(i+1 , target-arr[i] , arr);
+//         int subtract = helper(i+1 , target+arr[i] , arr);
+//         return add + subtract;
+//     }
+// }
