758bd5148eb2e3b70ef59b0cde16e0f594ae5b62
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-06-29 15:23:16 +0530

    Time: 28 ms (18.91%), Space: 53.8 MB (80.78%) - LeetHub

--- 1143-longest-common-subsequence/1143-longest-common-subsequence.java
+++ 1143-longest-common-subsequence/1143-longest-common-subsequence.java
@@ -1,15 +1,35 @@
+//Tabulation
 class Solution {
-    public int longestCommonSubsequence(String a, String b) {
+    public int longestCommonSubsequence(String a, String b)  {
         int m = a.length() , n = b.length();
         int[][] dp = new int[m][n];
-        for(int i =0 ; i< m; i++)
-            for(int j =0; j < n ; j++) dp[i][j] = -1; // -1 means unvisited
-        return lcs(m-1 , n-1 , a , b , dp); 
-    }
-    public int lcs(int i , int j , String a , String b , int[][] dp){
-        if(i < 0 || j < 0) return 0; //there is nothing in common
-        if(dp[i][j] != -1) return dp[i][j];
-        if(a.charAt(i) == b.charAt(j)) return dp[i][j] = 1 + lcs(i-1 , j-1 , a , b , dp);
-        else return dp[i][j] = Math.max(lcs(i-1 , j , a , b , dp) , lcs(i , j-1 , a , b , dp)); 
+        for(int i =0 ; i< m; i++){
+            for(int j =0; j < n ; j++){
+                int x = (i-1 >= 0 && j-1 >=0) ? dp[i-1][j-1] : 0;
+                int y = (i-1 >= 0) ? dp[i-1][j] : 0;
+                int z = (j-1 >= 0) ? dp[i][j-1] : 0;
+                if(a.charAt(i) == b.charAt(j)) dp[i][j] = 1 + x;
+                else dp[i][j] = Math.max(y , z);
+            }
+        }
+        return dp[m-1][n-1]; 
     }
 }
+
+//OR
+//Memoization
+// class Solution {
+//     public int longestCommonSubsequence(String a, String b) {
+//         int m = a.length() , n = b.length();
+//         int[][] dp = new int[m][n];
+//         for(int i =0 ; i< m; i++)
+//             for(int j =0; j < n ; j++) dp[i][j] = -1; // -1 means unvisited
+//         return lcs(m-1 , n-1 , a , b , dp); 
+//     }
+//     public int lcs(int i , int j , String a , String b , int[][] dp){
+//         if(i < 0 || j < 0) return 0; //there is nothing in common
+//         if(dp[i][j] != -1) return dp[i][j];
+//         if(a.charAt(i) == b.charAt(j)) return dp[i][j] = 1 + lcs(i-1 , j-1 , a , b , dp);
+//         else return dp[i][j] = Math.max(lcs(i-1 , j , a , b , dp) , lcs(i , j-1 , a , b , dp)); 
+//     }
+// }
