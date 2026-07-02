6b4fe61738ceb49010e8cf6d65b2abfa89bc61ef
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-07-02 18:24:07 +0530

    Time: 5 ms (87.96%), Space: 45.9 MB (92.01%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.java
@@ -1,24 +1,50 @@
-//Tabulation
+//Binary Search on Answer
 class Solution {
-    public int lengthOfLIS(int[] arr) {
-        int n = arr.length;
-        int[] dp = new int[n];
-        Arrays.fill(dp , 1);
-        int maxLen = 1;
-        for(int i = 1 ; i < n; i++){
-            int max = 0;
-            for(int j = 0 ; j < i; j++){
-                if(arr[j] < arr[i]){
-                    max = Math.max(max , dp[j]);
-                }
-            }
-            dp[i] += max;
-            maxLen = Math.max(maxLen , dp[i]);
+    public int lengthOfLIS(int arr[])  {
+        ArrayList<Integer> ans = new ArrayList<>();
+        for(int ele : arr){
+            if(ans.size() == 0 || ele > ans.get(ans.size()-1)) ans.add(ele);
+            else replace(ele , ans);
         }
-        return maxLen;
+        return ans.size();
+    }
+    public void replace(int ele , ArrayList<Integer> ans){
+        int lo = 0 , hi = ans.size()-1 , lb = -1; //replace with lower bound
+        while(lo <= hi){
+            int mid = lo + (hi-lo)/2;
+            if(ans.get(mid) >= ele){
+                lb = mid;
+                hi = mid-1;
+            }
+            else lo = mid+1;
+        }
+        ans.set(lb , ele);
     }
 }
 
+//OR
+//Tabulation
+// class Solution {
+//     public int lengthOfLIS(int[] arr) {
+//         int n = arr.length;
+//         int[] dp = new int[n];
+//         Arrays.fill(dp , 1);
+//         int maxLen = 1;
+//         for(int i = 1 ; i < n; i++){
+//             int max = 0;
+//             for(int j = 0 ; j < i; j++){
+//                 if(arr[j] < arr[i]){
+//                     max = Math.max(max , dp[j]);
+//                 }
+//             }
+//             dp[i] += max;
+//             maxLen = Math.max(maxLen , dp[i]);
+//         }
+//         return maxLen;
+//     }
+// }
+
+//OR
 // //Memoization
 // class Solution {
 //     public int lengthOfLIS(int[] arr) {
