a169debec27b3ab41bf95bca5ca3af039e195431
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-07-14 14:03:42 +0530

    Time: 0 ms (100%), Space: 42.3 MB (26.57%) - LeetHub

--- 0507-perfect-number/0507-perfect-number.java
+++ 0507-perfect-number/0507-perfect-number.java
@@ -1,10 +1,14 @@
 class Solution {
     public boolean checkPerfectNumber(int num) {
-        int sum = 0;
-        for(int i = 1; i <= num/2;i++){
-            if(num % i == 0) sum += i;
-        }
-        if(sum == num) return true;
-        return false;
+        // int sum = 0;
+        // for(int i = 1; i <= num/2;i++){
+        //     if(num % i == 0) sum += i;
+        // }
+        // if(sum == num) return true;
+        // return false;
+
+        //OR
+        if (num == 6 || num == 28 || num == 496 || num == 8128 || num == 33550336) return true;
+        else return false;
     }
 }
