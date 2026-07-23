b64060dede437b03147c08f1e30674be499023e9
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-07-24 00:00:55 +0530

    Time: 5 ms (49.44%), Space: 45.8 MB (99.85%) - LeetHub

--- 0001-two-sum/0001-two-sum.java
+++ 0001-two-sum/0001-two-sum.java
@@ -3,13 +3,13 @@ class Solution {
         HashMap<Integer, Integer> map = new HashMap<>();
         int[] ans = new int[2];
         for(int i = 0; i < nums.length; i++){
-        int rem = target - nums[i];
+            int rem = target - nums[i];
 
-        if(map.containsKey(rem)){
-            ans[0] = map.get(rem);
-            ans[1] = i;
-        }
-        map.put(nums[i], i);
+            if(map.containsKey(rem)){
+                ans[0] = map.get(rem);
+                ans[1] = i;
+            }
+            map.put(nums[i], i);
         }
         return ans;
     }
