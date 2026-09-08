40490aa315311e1720773e683bc7ba870a7c5dac
Swapnil Dwivedi <dwivediswapnil73@gmail.com>
2026-09-08 21:24:16 +0530

    Time: 1 ms (83.65%), Space: 42.7 MB (39.85%) - LeetHub

--- 3870-count-commas-in-range/3870-count-commas-in-range.java
+++ 3870-count-commas-in-range/3870-count-commas-in-range.java
@@ -1,16 +1,24 @@
+// class Solution {
+//     public int countCommas(int n) {
+//         int ans = noOfDigits(n);
+//         if(ans < 4) return 0;
+//         else return n - 999;
+//     }
+//     public int noOfDigits(int n){
+//         int count = 0;
+//         while(n > 0){
+//             int ld = n % 10;
+//             count++;
+//             n /= 10;
+//         }
+//         return count;
+//     }
+// }
+
+//OR
+
 class Solution {
     public int countCommas(int n) {
-        int ans = noOfDigits(n);
-        if(ans < 4) return 0;
-        else return n - 999;
-    }
-    public int noOfDigits(int n){
-        int count = 0;
-        while(n > 0){
-            int ld = n % 10;
-            count++;
-            n /= 10;
-        }
-        return count;
+        return Math.max(0, n - 999);
     }
 }
